363f71e2a323d145c3c0c294520b54296d8fa1bb
WilliamsAl282 <WilliamsAl282@northwested.org>
2025-05-09 10:07:31 -0400

    work

--- main.py
+++ main.py
@@ -4,4 +4,21 @@
 
 
 
+# A simple class that models a bank account
+class BankAccount:
+
+    def __init__(self,account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self,amount):
+        if amount > 0:
+            self.balance + amount
+            print(f'The added fund of {amount} was added to your {self.balance}')
+        else:
+
+
+
+
+
 
